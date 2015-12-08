4a05ca20dd1b5a40695b93a41f3dcd394d98ad09
Maksim S Makarov <m.s.makarov@gmail.com>
2015-12-08 10:56:08 +0300

    Инициализация проекта

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'app',
+    'password' => 'H3xCmAY2PFubMbb9',
     'charset' => 'utf8',
 ];
