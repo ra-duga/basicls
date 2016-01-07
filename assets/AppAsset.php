0030faea7ef3e947eaab7c0eac8af8859978beea
Maksim S Makarov <m.s.makarov@gmail.com>
2016-01-07 16:39:12 +0300

    nil

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,6 +21,7 @@ class AppAsset extends AssetBundle
         'css/site.css',
     ];
     public $js = [
+        'js/bootstrap.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
