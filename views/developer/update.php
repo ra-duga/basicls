fde9bb87fa083c08b5558abd2da7f50e459a6762
Maksim S Makarov <m.s.makarov@gmail.com>
2016-01-07 16:39:46 +0300

    nil

--- views/developer/update.php
+++ views/developer/update.php
@@ -5,14 +5,13 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Developers */
 
-$this->title = 'Update Developers: ' . ' ' . $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Developers', 'url' => ['index']];
+$this->title = 'Редактировать застрощика: ' . ' ' . $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Застройщик', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Редактировать';
 ?>
 <div class="developers-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
