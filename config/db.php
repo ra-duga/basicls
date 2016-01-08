7dd026e3f906a61aad21851eca165db4fe9ffa75
Maksim S Makarov <m.s.makarov@gmail.com>
2016-01-08 21:05:56 +0200

    nil

--- config/db.php
+++ config/db.php
@@ -3,12 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-<<<<<<< HEAD
     'username' => 'app3',
     'password' => 'xTnAwjDZh3Z4cwv9',
-=======
-    'username' => 'app',
-    'password' => 'H3xCmAY2PFubMbb9',
->>>>>>> Тестирование
     'charset' => 'utf8',
 ];
